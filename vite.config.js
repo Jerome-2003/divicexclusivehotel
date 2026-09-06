d7fa94475e68199648d30b02fcbd88d95b1c6088
Ugo <nuchukwuma1@gmail.com>
2026-09-06 19:36:19 +0100

    404 fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist', sourcemap: false },
+  base: "/divicexclusivehotel/",
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
+
